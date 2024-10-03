ef21a032c7bdf3e3b9407e251170221359d052e9
yazin010 <yazinbj@gmail.com>
2024-10-03 16:33:59 +0530

    day7

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Expenses(),
+  runApp(MaterialApp(
+    theme: ThemeData (useMaterial3: true),
+    home: const Expenses(),
   ));
 }
